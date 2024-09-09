f0ff19f6b715d46ae717dd9fce024df7b620d6ab
itsmearafik <arafikalhassan@gmail.com>
2024-09-09 18:50:33 +0100

    updated api tests v.1

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,21 +1,12 @@
-# api/models.py
 from django.db import models
 
 class Author(models.Model):
-    # Represents an author with a name field
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
-
-
+    
 class Book(models.Model):
-    # Represents a book with a title, publication year, and an author
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,11 +1,10 @@
-# api/urls.py
 from django.urls import path
-from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .views import CustomBookCreateView, CustomBookListView, CustomBookDetailView, CustomBookUpdateView, CustomBookDeleteView
 
 urlpatterns = [
-    path('books/', ListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),
-    path('books/create/', CreateView.as_view(), name='book-create'),
-    path('books/update/', UpdateView.as_view(), name='book-update'),
-    path('books/delete/', DeleteView.as_view(), name='book-delete'),
+    path('books/', CustomBookListView.as_view(), name='list'),
+    path('books/<int:pk>', CustomBookDetailView.as_view(), name='detail'),
+    path('books/update/<int:pk>', CustomBookUpdateView.as_view(), name='update'),
+    path('books/delete/<int:pk>', CustomBookDeleteView.as_view(), name='delete'),
+    path('books/create', CustomBookCreateView.as_view(), name='create'),
 ]
